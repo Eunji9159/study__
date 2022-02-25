74c952b9e7d70957ca658436121fb2dff7ec516a
Eunji9159 <ejhong1105@naver.com>
2022-02-25 19:14:29 +0900

    first

---
+++ Machine.js
@@ -0,0 +1,17 @@
+class Machine {
+    constructor(){
+
+    }
+
+    plus (a, b) {
+        return a + b
+    }
+
+    minus (a, b) {
+        return a - b
+    }
+
+    divide (a, b){
+        return a/b
+    }
+}
---
+++ test01.js
@@ -0,0 +1,5 @@
+import Machine from './Machine'
+
+// void static main 
+let mc1 = new Machine();
+console.log(mc1.plus(1,2));
